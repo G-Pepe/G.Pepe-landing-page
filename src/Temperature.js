1fa665fc0c40e810f405cfd8b3be39698d10eed3
Gabriele Pepe <gabriele.pepe@gmail.com>
2020-12-07 13:47:47 +0100

    added variable into netlify env and modified spelling mistake in API key

--- src/Temperature.js
+++ src/Temperature.js
@@ -5,16 +5,20 @@ export const Temperature = () => {
   const { weather } = useContext(MyContext);
   return (
     <div>
-      <h2>{weather.name}</h2>
-      <div>
-        <img
-          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-          alt=""
-        />
-      </div>
-      <h3>Temperature: {weather.main.temp}</h3>
-      <p>Max: {weather.main.temp_max} </p>
-      <p>Min: {weather.main.temp_min}</p>
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt=""
+            />
+          </div>
+          <h3>Temperature: {weather.main.temp}</h3>
+          <p>Max: {weather.main.temp_max} </p>
+          <p>Min: {weather.main.temp_min}</p>
+        </div>
+      )}
     </div>
   );
 };
